565904cec614b9acd620e8e187ada219cd192979
DaniloBarros <dan.b412@gmail.com>
2016-10-27 09:26:08 -0200

    Addedddded training for zellus sp

--- trainCascade.sh
+++ trainCascade.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Danilo Barros Mendes
 # Under license APGLv3
-find positive -name '*.jpg' -exec identify -format '%i 1 0 0 %w %h' \{\} \; > positives.lst
+find positive -name '*.jpg' -exec identify -format '%i 1 0 0 %w %h\n' \{\} \; > positives.lst
 
 opencv_createsamples -info positives.lst -vec positives.vec -h 20 -w 20
 
 echo "Try to train now."
 
-opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 115 -numNeg 1000 -numStage 15 -w 20 -h 20 -min -precalcValBufSize 6000 -precalcIdxBufSize 6000 -mode ALL
+opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 400 -numNeg 1200 -numStage 15 -w 20 -h 20 -precalcValBufSize 6000 -precalcIdxBufSize 6000 -mode ALL
 
 # Alternative training
 # for i in {1..133}
